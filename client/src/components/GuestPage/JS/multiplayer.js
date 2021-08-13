14fa966f87d8485e7148031a4db7c7abbc0a5e1d
Petar Dosev <pdosev@gmail.com>
2021-08-13 16:57:10 -0400

    Production config

--- client/src/components/GuestPage/JS/multiplayer.js
+++ client/src/components/GuestPage/JS/multiplayer.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-let socket = io.connect('http://localhost:5000');
+let socket = io.connect(process.env.REACT_APP_SOCKET_SERVER + process.env.REACT_APP_SOCKET_PORT);
 
 export default socket
 
