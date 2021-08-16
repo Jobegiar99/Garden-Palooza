63875ee9e835366eab6a99ac6a9869408213b320
Petar Dosev <pdosev@gmail.com>
2021-08-16 17:10:45 -0400

    Fix mixed content error

--- client/src/components/GuestPage/JS/multiplayer.js
+++ client/src/components/GuestPage/JS/multiplayer.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-let socket = io.connect('http://' + document.domain + ':443/socket');
+let socket = io.connect('https://' + document.domain + '/socket');
 
 export default socket
 
