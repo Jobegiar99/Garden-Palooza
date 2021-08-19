b3ad861aacb67eac12bd3d4cdd5721ded8cb9304
Petar Dosev <pdosev@gmail.com>
2021-08-19 16:43:08 -0400

    Fixed bug with socket server not sending messages

--- client/src/components/GuestPage/JS/multiplayer.js
+++ client/src/components/GuestPage/JS/multiplayer.js
@@ -1,7 +1,8 @@
 import { io } from "socket.io-client";
 
-console.log("Connecting to socket server at " + process.env.REACT_APP_SOCKET_URL);
-let socket = io.connect(process.env.REACT_APP_SOCKET_URL);
+const socketUrl = process.env.NODE_ENV === 'production' ? 'https://gardenpalooza.tech/' : 'http://localhost:5000';
+console.log("Connecting to socket server at " + socketUrl);
+let socket = io.connect(socketUrl);
 
 export default socket
 
